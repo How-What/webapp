60c1d57c0883e76bfaed342385d2860074af8731
Wallace Wong <walwong@JOCA-105-1030-M.local>
2018-07-11 11:54:48 -0400

    Added forms to the commit
    Created a form in the users page

--- app/route.py
+++ app/route.py
@@ -1,16 +1,17 @@
 from flask import render_template
 from app import app
+from app.form import Login
 
 @app.route('/', methods = ['GET'])
 @app.route('/home', methods = ['GET'])
 def home():
     return "Hello World!"
 
-@app.route('/<string:user>', methods=['GET'])
+@app.route('/<string:user>', methods=['GET','POST'])
 def user_profile(user):
     '''
         This functions take in user from url bar and outputs on the page
         
         :param user: takes in user's name from url
     '''
-    return render_template('user.html', user= user)
+    return render_template('user.html', user= user, form = Login())
