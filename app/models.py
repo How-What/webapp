3114fd61359cc0adc8722107df1d987ffa821c55
Wallace Wong <walwong@JOCA-105-1030-M.local>
2018-07-12 10:12:16 -0400

    Added login and logout

--- app/models.py
+++ app/models.py
@@ -1,7 +1,8 @@
-from app import db
+from app import db, login
+from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index = True, unique  = True)
     password_hash = db.Column(db.String(150))
@@ -11,8 +12,19 @@ class User(db.Model):
         password_hash = self.password_hash
 
     def set_password(self, password):
+        '''
+            generates the password has generated and sets it
+
+            :password_hash: the password has generated
+        '''
         self.password_hash = generate_password_hash(password)
     
     def check_password(self, password):
+        '''
+            checks the passwords and see if it matches returns true if it does
+        '''
         return check_password_hash(self.password_hash, password)
     
+    @login.user_loader
+    def load_user(id):
+        return User.query.get(int(id)) 
--- app/route.py
+++ app/route.py
@@ -1,4 +1,5 @@
-from flask import render_template, redirect, url_for
+from flask import flash, render_template, redirect, url_for
+from flask_login import current_user, login_user, logout_user
 from app import app, db
 from app.form import Login
 from app.models import User
@@ -8,6 +9,7 @@ from app.models import User
 def home():
     return "Hello World!"
 
+
 @app.route('/<string:user>', methods=['GET','POST'])
 def user_profile(user):
     '''
@@ -15,13 +17,32 @@ def user_profile(user):
         
         :param user: takes in user's name from url
     '''
+    return render_template('user.html', title = User, user = user)
 
-    form = Login()
-    user_data = User(username = form.username.data, password_hash = form.password.data)
-  
-    if form.validate_on_submit():
-        db.session.add(user_data)
-        db.session.commit()
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    
+    if current_user.is_authenticated:
         return redirect(url_for('home'))
     
-    return render_template('user.html', user= user, form = Login())
+    form = Login()
+    user_data = User.query.filter_by(username = form.username.data).first()
+    if form.validate_on_submit():
+        if user is None or not user.check_password(form.password.data):
+            flash("Invalid Username or Passowrd")
+            return redirect(url_for("login"))
+        login_user(user)
+        return redirect(url_for('home'))
+  
+    return render_template('login.html', title = Login, form = form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+
+@app.route('/signup', methods=['POST'])
+def regesiter
